d8e2a059f0db2feba074035f333486bd650dca3c
JoshuaHamann <joshwashku@yahoo.com>
2022-11-08 09:13:57 -0600

    added generatePassword function shell, added a length prompt to said function

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,23 @@ function writePassword() {
 
 }
 
+// Algorithm to generate password
+var generatePassword = function () {
+    // var passLength = window.prompt("Type a number to generate the length of the password.");
+
+    var aNumber = Number(window.prompt("Type a number to generate the length of the password.", ""));
+    // Checks to see if passLength is a number, then either moves on to the next prompt, or
+    // sends a window alert to ensure the user types in a valid number.
+    if(isNaN(aNumber)){
+        window.alert("Please type in a number.");
+        return;
+    }
+
+    // var passLowercase = window.prompt("Would you like to include ")
+
+    
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
